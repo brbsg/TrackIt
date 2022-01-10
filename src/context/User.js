2db4ace17131fc8093818b4d5ba630bf699f97bb
barbosaoli <gustavobarbosa7162@gmail.com>
2022-01-10 16:44:10 -0300

    finalizacao

--- src/context/User.js
+++ src/context/User.js
@@ -4,14 +4,12 @@ const UserContext = createContext();
 
 export function UserProvider({ children }) {
   const [user, setUser] = useState({
-    id: 1359,
-    name: "Gustavo",
-    image:
-      "https://i.picsum.photos/id/38/200/300.jpg?hmac=-3xmMd1qccZR3fLPMvwj8D3GgMIIDCKTpXJspTKuZW0",
-    email: "gustavob@gmail.com",
-    password: "1234",
-    token:
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTM1OSwiaWF0IjoxNjQxNzYwMjEyfQ.EJl8AtHnlT1R_HjD0tbIUzhiqB2QHIx1RKAq9GxV6eE",
+    id: 0,
+    name: "",
+    image: "",
+    email: "",
+    password: "",
+    token: "",
   });
   const [update, setUpdate] = useState(false);
   const [progress, setProgress] = useState(0);
